9c5bf088d5d7e00ee3993b11832c317573d9ab15
Kamel Cerbah <ren.cerbah@gmail.com>
2022-06-22 11:07:57 +0100

    initial vue project files

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container mt-5 mx-auto text-center">
+    <nav>
+      <ul class="nav justify-content-center">
+        <li class="nav-item mx-2">
+          <router-link class="btn btn-outline-success" to="/">Home</router-link>
+        </li>
+        <li class="nav-item mx-2">
+          <router-link class="btn btn-outline-success" to="/about">About</router-link>
+        </li>
+      </ul>
+    </nav>
+
+    <router-view />
+  </div>
+</template>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+} */
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home ">
+    <h1 class="h1 fw-light">This is a home page</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
